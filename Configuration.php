fb9b9447cc95a1d961cb2173525d1fcc635624ae
Vladislav Ershov <ershov.v@tecomgroup.ru>
2016-06-27 12:28:39 +0300

    configuration class

---
+++ Configuration.php
@@ -0,0 +1,47 @@
+<?php
+/*
+* Copyright (c) 2016 Tecom LLC
+* All rights reserved
+*
+* Исключительное право (c) 2016 пренадлежит ООО Теком
+* Все права защищены
+*/
+/**
+Класс Singleton
+
+@author ershov.v
+*/
+class Configuration
+{
+	public $config = array();
+
+	function __construct() {
+	}
+
+	/** Фиксирование подключения к базе данных PostgreSQL.*/
+	public static function instance()
+	{
+		static $instance = null;
+		if ($instance === null) {
+			$instance = new Configuration();
+			$instance->readConfigAndDBConnection();
+		}
+		return $instance;
+	}
+	
+	/** Чтение переменных из файла config.php и подключение к базе данных.*/
+	private function readConfigAndDBConnection()
+	{
+		$dbConnect = null;
+
+		$dbConnect = pg_connect($host, $port, $user, $password, $dbName);
+		if (!$dbConnect) {
+			throw new Exception("Не удается подключится к базе данных");
+		}
+		return $dbConnect;
+	}
+}
+
+
+Configuration::instance()->config;
+?>
