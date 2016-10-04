c02fc02c6d76d0572f3ca9d6e6689b7f2745abad
Vladislav <ershov.v@tecomgroup.ru>
2016-10-04 13:58:48 +0300

    Откат

---
+++ templates_c/417b74d74b047b733725e690af3f61ebe15e79a3_0.file.listEmployees.tpl.php
@@ -0,0 +1,499 @@
+<?php
+/* Smarty version 3.1.28, created on 2016-10-04 13:51:26
+  from "/var/www/hr-timetrack/3pty/Smarty/demo/templates/listEmployees.tpl" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.28',
+  'unifunc' => 'content_57f389ae6581a6_70089345',
+  'file_dependency' => 
+  array (
+    '417b74d74b047b733725e690af3f61ebe15e79a3' => 
+    array (
+      0 => '/var/www/hr-timetrack/3pty/Smarty/demo/templates/listEmployees.tpl',
+      1 => 1475499023,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:3pty/Smarty/demo/templates/header.tpl' => 1,
+  ),
+),false)) {
+function content_57f389ae6581a6_70089345 ($_smarty_tpl) {
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Tecomgroup | <?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+</title>
+		<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+	</head>
+	<body style="height:100%;" class="hold-transition skin-blue sidebar-mini" <?php echo $_smarty_tpl->tpl_vars['errorAlert']->value;?>
+>
+		<div class="wrapper">
+			<?php $_smarty_tpl->smarty->ext->_subtemplate->render($_smarty_tpl, "file:3pty/Smarty/demo/templates/header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+
+			<div class="content-wrapper">
+				<section class="content">
+					<div class="row">
+						<div class="col-xs-12">
+						<?php if ($_smarty_tpl->tpl_vars['statusEditing']->value != null) {?>
+						<?php echo $_smarty_tpl->tpl_vars['statusEditing']->value;?>
+
+						<?php }?>
+							<div class="box">
+								<div class="box-header">
+									<h3 class="box-title" style="font-size:23px">Список Сотрудников</h3>	
+								</div>
+								<div class="box-body">
+									<table id="employeeList" class="table table-bordered table-striped">
+										<thead>
+											<tr>
+												<th>Фамилия и Имя</th>
+												<th>Отдел</th>
+												<th style="width: 18px"></th>
+												<th style="width: 18px"></th>
+											</tr>
+										</thead>
+										<tbody>
+										<?php if ($_smarty_tpl->tpl_vars['arrayEmployeeNames']->value != null) {?>
+										<?php
+$_from = $_smarty_tpl->tpl_vars['arrayEmployeeNames']->value;
+if (!is_array($_from) && !is_object($_from)) {
+settype($_from, 'array');
+}
+$__foreach_foo_0_saved_item = isset($_smarty_tpl->tpl_vars['foo']) ? $_smarty_tpl->tpl_vars['foo'] : false;
+$_smarty_tpl->tpl_vars['foo'] = new Smarty_Variable();
+$__foreach_foo_0_total = $_smarty_tpl->smarty->ext->_foreach->count($_from);
+if ($__foreach_foo_0_total) {
+foreach ($_from as $_smarty_tpl->tpl_vars['foo']->value) {
+$__foreach_foo_0_saved_local_item = $_smarty_tpl->tpl_vars['foo'];
+?>
+										
+											<tr>
+												<td>
+													<a 
+														href="/index.php
+															?route=Employee/viewEmployee
+															&employeeId=<?php echo $_smarty_tpl->tpl_vars['foo']->value['employee_id'];?>
+
+															&employeeName=<?php echo $_smarty_tpl->tpl_vars['foo']->value['user_name'];?>
+
+															&employeeLogin=<?php echo $_smarty_tpl->tpl_vars['foo']->value['user_id'];?>
+
+															&departmentId=<?php echo $_smarty_tpl->tpl_vars['foo']->value['department_id'];?>
+
+															&departmentName=<?php echo $_smarty_tpl->tpl_vars['foo']->value['department_name'];?>
+
+															&Month=<?php echo $_smarty_tpl->tpl_vars['selectedMonthForGet']->value;?>
+
+															&Year=<?php echo $_smarty_tpl->tpl_vars['selectedYearForGet']->value;?>
+">
+														<?php echo $_smarty_tpl->tpl_vars['foo']->value['user_name'];?>
+
+													</a>
+												</td>
+												<td>
+													<a 
+														href="/index.php
+															?route=Department/viewDepartment
+															&departmentId=<?php echo $_smarty_tpl->tpl_vars['foo']->value['department_id'];?>
+
+															&departmentName=<?php echo $_smarty_tpl->tpl_vars['foo']->value['department_name'];?>
+
+															&Month=<?php echo $_smarty_tpl->tpl_vars['selectedMonthForGet']->value;?>
+
+															&Year=<?php echo $_smarty_tpl->tpl_vars['selectedYearForGet']->value;?>
+">
+														<?php echo $_smarty_tpl->tpl_vars['foo']->value['department_name'];?>
+
+													</a>
+												</td>
+												<td>
+													<?php if ($_smarty_tpl->tpl_vars['status']->value == FALSE) {?>
+													<a 
+														type="button" 
+														class="btn btn-md" 
+														<?php if ($_smarty_tpl->tpl_vars['access']->value == null) {?>
+														data-toggle="modal" 
+														data-editid="<?php echo $_smarty_tpl->tpl_vars['foo']->value['employee_id'];?>
+"
+														data-lastlogin="<?php echo $_smarty_tpl->tpl_vars['foo']->value['user_id'];?>
+" 
+														data-lastname="<?php echo $_smarty_tpl->tpl_vars['foo']->value['user_name'];?>
+" 
+														data-departmentid="<?php echo $_smarty_tpl->tpl_vars['foo']->value['department_id'];?>
+" 
+														data-countselect="<?php echo $_smarty_tpl->tpl_vars['countArrayDepartmentNamesForSelect']->value;?>
+"  
+														data-target="#employeeModal" 
+														<?php }?>
+														title="Редактировать Данные Сотрудника"
+														<?php echo $_smarty_tpl->tpl_vars['access']->value;?>
+>
+														<i class="glyphicon glyphicon-pencil"></i>
+													</a>
+													<?php }?>
+												</td>
+												<td>
+													<?php if ($_smarty_tpl->tpl_vars['status']->value == FALSE) {?>
+													<a 
+														type="button" 
+														class="btn btn-md" 
+														<?php if ($_smarty_tpl->tpl_vars['access']->value == null) {?>
+														data-toggle="modal" 
+														data-employeeid="<?php echo $_smarty_tpl->tpl_vars['foo']->value['employee_id'];?>
+" 
+														data-employeename="<?php echo $_smarty_tpl->tpl_vars['foo']->value['user_name'];?>
+" 
+														data-target="#removeModal" 
+														<?php }?>
+														title="Удалить Данные Сотрудника"
+														<?php echo $_smarty_tpl->tpl_vars['access']->value;?>
+>
+														<i class="glyphicon glyphicon-trash"></i>
+													</a>
+													<?php }?>
+												</td>
+											</tr>
+										<?php
+$_smarty_tpl->tpl_vars['foo'] = $__foreach_foo_0_saved_local_item;
+}
+}
+if ($__foreach_foo_0_saved_item) {
+$_smarty_tpl->tpl_vars['foo'] = $__foreach_foo_0_saved_item;
+}
+?>
+										<?php }?>
+										</tbody>
+									</table>
+									<?php if ($_smarty_tpl->tpl_vars['status']->value == FALSE) {?>
+									<a 
+										type="button" 
+										class="btn btn-md" 
+										<?php if ($_smarty_tpl->tpl_vars['access']->value == null) {?>
+										data-toggle="modal" 
+										data-countselect="<?php echo $_smarty_tpl->tpl_vars['countArrayDepartmentNamesForSelect']->value;?>
+" 
+										data-target="#employeeModal" 
+										<?php }?>
+										title="Добавить Сотрудника"
+										<?php echo $_smarty_tpl->tpl_vars['access']->value;?>
+>
+										<i class="glyphicon glyphicon-plus"></i>
+									</a>
+									<?php }?>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="modal fade" id="employeeModal" role="dialog" aria-labelledby="employeeModalLabel">
+						<div class="modal-dialog" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+										<span aria-hidden="true">&times;</span>
+									</button>
+									<h4 class="modal-title" id="employeeModalLabel"></h4>
+								</div>
+								<form action="/index.php" method="get" onsubmit="diactive()">
+									<div class="modal-body">
+										<div class="modal-form-group">
+											<div class="row">
+												<div class="col-xs-4">
+													<label for="nameEmployeeS">Фамилия</label>
+													<input 
+														id="nameEmployeeS" 
+														name="nameEmployeeS"
+														type="text" 
+														class="form-control" 
+														required="required">
+												</div>
+												<div class="col-xs-4">
+													<label for="nameEmployeeF">Имя</label>
+													<input 
+														id="nameEmployeeF" 
+														name="nameEmployeeF" 
+														type="text" 
+														class="form-control" 
+														required="required">
+												</div>
+												<div class="col-xs-4">
+													<label for="nameEmployeeM">Отчество</label>
+													<input 
+														id="nameEmployeeM" 
+														name="nameEmployeeM"
+														type="text" 
+														class="form-control"
+														required="required">
+												</div>
+											</div>
+										</div>
+										<div class="form-group">
+											<label>Логин:</label>
+											<input  
+												id="loginEmployee"
+												name="newLogin" 
+												type="text" 
+												class="form-control"
+												required="required">
+										</div>
+										<div class="form-group">
+											<label>Отдел:</label>
+											<select 
+												id="selectId" 
+												name="newDepartment" 
+												class="form-control select2" 
+												style="width: 100%;"
+												required="required">
+											<?php
+$_from = $_smarty_tpl->tpl_vars['arrayDepartmentNamesForSelect']->value;
+if (!is_array($_from) && !is_object($_from)) {
+settype($_from, 'array');
+}
+$__foreach_foo_1_saved_item = isset($_smarty_tpl->tpl_vars['foo']) ? $_smarty_tpl->tpl_vars['foo'] : false;
+$_smarty_tpl->tpl_vars['foo'] = new Smarty_Variable();
+$__foreach_foo_1_total = $_smarty_tpl->smarty->ext->_foreach->count($_from);
+if ($__foreach_foo_1_total) {
+foreach ($_from as $_smarty_tpl->tpl_vars['foo']->value) {
+$__foreach_foo_1_saved_local_item = $_smarty_tpl->tpl_vars['foo'];
+?>
+											
+												<option value="<?php echo $_smarty_tpl->tpl_vars['foo']->value['department_id'];?>
+"><?php echo $_smarty_tpl->tpl_vars['foo']->value['department_name'];?>
+</option>
+											<?php
+$_smarty_tpl->tpl_vars['foo'] = $__foreach_foo_1_saved_local_item;
+}
+}
+if ($__foreach_foo_1_saved_item) {
+$_smarty_tpl->tpl_vars['foo'] = $__foreach_foo_1_saved_item;
+}
+?>
+											</select>
+										</div>
+									</div>
+									<div class="modal-footer">
+										<div class="input-group hidden">
+											<input id="route" name="route" type="hidden">
+											<input id="editId" name="editId" type="hidden">
+											<input name="Month" type="hidden" value="<?php echo $_smarty_tpl->tpl_vars['selectedMonthForGet']->value;?>
+">
+											<input name="Year" type="hidden" value="<?php echo $_smarty_tpl->tpl_vars['selectedYearForGet']->value;?>
+">
+										</div>
+										<button id="buttonModalF" style="width: 200px" type="button" class="btn btn-default pull-left" data-dismiss="modal">Отмена</button>
+										<button id="buttonModalS" style="width: 200px" type="submit" class="btn btn-primary">Сохранить</button>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+					<div class="modal fade" id="removeModal" tabindex="-1" role="dialog" style="margin: 0 auto;">
+						<div class="modal-dialog" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+										<span aria-hidden="true">&times;</span>
+									</button>
+									<h4 class="modal-title" id="removeModalLabel"></h4>
+								</div>							
+								<form action="/index.php" method="get" onsubmit="diactiveRemove()">
+									<div class="modal-body">
+									</div>
+									<div class="modal-footer">
+										<div class="input-group hidden">
+											<input name="route" type="hidden" value="List/removeEmployee">
+											<input id="employeeId" name="employeeId" type="hidden">
+											<input name="Month" type="hidden" value="<?php echo $_smarty_tpl->tpl_vars['selectedMonthForGet']->value;?>
+">
+											<input name="Year" type="hidden" value="<?php echo $_smarty_tpl->tpl_vars['selectedYearForGet']->value;?>
+">
+										</div>
+										<button id="buttonModalFRemove" type="button" class="btn btn-default pull-left" data-dismiss="modal" style="width: 200px">Отмена</button>
+										<button id="buttonModalSRemove" type="submit" class="btn btn-primary" style="width: 200px">Да</button>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+					<div class="modal fade" id="changeDataMonthEditing" tabindex="-1" role="dialog" style="margin: 0 auto;">
+						<div class="modal-dialog" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+										<span aria-hidden="true">&times;</span>
+									</button>
+									<h4 class="modal-title" id="changeDataMonthEditingLabel">Вы уверены что хотите 
+										<?php if ($_smarty_tpl->tpl_vars['statusEditing']->value == null) {?>
+											заблокировать
+										<?php }?>
+										<?php if ($_smarty_tpl->tpl_vars['statusEditing']->value != null) {?>
+											разблокировать
+										<?php }?>
+										 данные месяца <b>(<?php echo $_smarty_tpl->tpl_vars['selectedMonth']->value;?>
+-<?php echo $_smarty_tpl->tpl_vars['selectedYearForGet']->value;?>
+)</b> для редактирования.
+									</h4>
+								</div>							
+								<form action="/index.php" method="get" onsubmit="diactiveChangeDataMonthEditing()">
+									<div class="modal-body">
+									</div>
+									<div class="modal-footer">
+										<div class="input-group hidden">
+											<input name="route" type="hidden" value="List/changeDataStatusForEditing">
+											<input name="lastStatus" type="hidden" value="<?php echo $_smarty_tpl->tpl_vars['status']->value;?>
+">
+											<input name="lastPage" type="hidden" value="Employee">
+											<input name="Month" type="hidden" value="<?php echo $_smarty_tpl->tpl_vars['selectedMonthForGet']->value;?>
+">
+											<input name="Year" type="hidden" value="<?php echo $_smarty_tpl->tpl_vars['selectedYearForGet']->value;?>
+">
+										</div>
+										<button id="buttonModalFData" type="button" class="btn btn-default pull-left" data-dismiss="modal" style="width: 200px">Отмена</button>
+										<button id="buttonModalSData" type="submit" class="btn btn-primary" style="width: 200px">Да</button>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+			<footer class="main-footer">
+				<div class="pull-right hidden-xs">
+				</div>
+			</footer>
+			<?php echo '<script'; ?>
+>
+				$('#employeeModal').on('show.bs.modal', function (event) {
+					var button = $(event.relatedTarget);
+					var modal = $(this);
+					var editId = button.data('editid');
+					var lastName = button.data('lastname');
+					var lastLogin = button.data('lastlogin');
+					var departmentId = button.data('departmentid');
+					var countSelect = button.data('countselect');
+					for (var i = 0; i < countSelect; i++) {
+						var val = document.getElementById('selectId').options[i].value;
+						if (val == departmentId){
+							document.getElementById('selectId').options[i].selected=true;
+						}else{
+							document.getElementById('selectId').options[i].selected=false;
+						}
+					}
+					if (editId != null){
+						modal.find('.modal-title').text('Редактировать Данные Сотрудника');
+						document.getElementById('route').value = 'List/editEmployee';
+						document.getElementById('editId').value = editId;
+						lastName=lastName.split(' ');
+						document.getElementById('nameEmployeeF').value = lastName[1];
+						document.getElementById('nameEmployeeS').value = lastName[0];
+						document.getElementById('nameEmployeeM').value = lastName[2];
+						document.getElementById('loginEmployee').value = lastLogin;
+					}else{
+						modal.find('.modal-title').text('Новый Сотрудник');
+						document.getElementById('route').value = 'List/newEmployee';						
+						document.getElementById('editId').value = null;
+						document.getElementById('nameEmployeeF').value = null;
+						document.getElementById('nameEmployeeS').value = null;
+						document.getElementById('nameEmployeeM').value = null;
+						document.getElementById('loginEmployee').value = null;
+					}
+					$(function () {
+						$(".select2").select2({
+						modal: true,
+						placeholder: "Выберите Отдел",
+						allowClear: true
+						});
+					});
+				});
+			<?php echo '</script'; ?>
+>
+			<?php echo '<script'; ?>
+>
+				function diactive() {
+					document.getElementById('buttonModalS').disabled = 1;
+					document.getElementById('buttonModalF').disabled = 1;
+					document.getElementById("loginEmployee").setAttribute("readonly", "readonly");
+					document.getElementById("nameEmployeeM").setAttribute("readonly", "readonly");
+					document.getElementById("nameEmployeeF").setAttribute("readonly", "readonly");
+					document.getElementById("nameEmployeeS").setAttribute("readonly", "readonly");
+				}
+			<?php echo '</script'; ?>
+>
+			<?php echo '<script'; ?>
+>
+				$('#removeModal').on('show.bs.modal', function (event) {
+					var button = $(event.relatedTarget);
+					var modal = $(this);
+					var employeeId = button.data('employeeid');
+					var employeeName = button.data('employeename');
+					modal.find('.modal-title').html('Вы уверены, что хотите удалить данные сотрудника: <u><b>'+employeeName+'</u></b>');
+					document.getElementById('employeeId').value = employeeId;
+				});
+			<?php echo '</script'; ?>
+>
+			<?php echo '<script'; ?>
+>
+				function diactiveRemove() {
+					document.getElementById('buttonModalSRemove').disabled = 1;
+					document.getElementById('buttonModalFRemove').disabled = 1;
+				}
+			<?php echo '</script'; ?>
+>
+			<?php echo '<script'; ?>
+>
+				function diactiveChangeDataMonthEditing() {
+					document.getElementById('buttonModalSData').disabled = 1;
+					document.getElementById('buttonModalFData').disabled = 1;
+				}
+			<?php echo '</script'; ?>
+>
+			<?php echo '<script'; ?>
+>
+				function errorMassage() {
+					alert('<?php echo $_smarty_tpl->tpl_vars['errorMessage']->value;?>
+');
+				};
+			<?php echo '</script'; ?>
+>
+			<?php echo '<script'; ?>
+>
+				$(function () {
+					$('#employeeList').DataTable({
+						"paging": true,
+						"lengthChange": true,
+						"searching": true,
+						"ordering": true,
+						"info": true,
+						"autoWidth": true,
+						"stateSave": true,
+						"stateDuration": -1,
+						"language": {
+							"lengthMenu": "Показать _MENU_ элементов",
+							"zeroRecords": "Ничего не найдено",
+							"info": "С _START_ по _END_ из _TOTAL_",
+							"infoEmpty": "Нет доступных данных",
+							"infoFiltered": "(Отфильтровано из _MAX_ записей)",
+							"paginate": {
+								"first":"Первый",
+								"last": "Последний",
+								"next": "Следующая",
+								"previous": "Предыдущая"
+								},
+							"loadingRecords": "Загрузка...",
+							"processing": "Обработка...",
+							"search": "Поиск:"
+						}
+					});
+				});
+			<?php echo '</script'; ?>
+>
+		</body>
+</html>
+<?php }
+}
